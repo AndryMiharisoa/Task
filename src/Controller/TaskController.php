f9905945f9038f47e2e2b1a5be028a60687dca44
AndryMihary <miharyandrianjafimanana@gmail.com>
2024-10-24 16:30:51 +0200

    1er push

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,17 +2,76 @@
 
 namespace App\Controller;
 
+use App\Entity\Todo;
+use App\Form\TodoType;
+use App\Repository\TodoRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class TaskController extends AbstractController
 {
     #[Route('/task', name: 'app_task')]
-    public function index(): Response
+    public function index(TodoRepository $todoRepository): Response
     {
         return $this->render('task/index.html.twig', [
-            'controller_name' => 'TaskController',
+            'todos' => $todoRepository->findAll(),
         ]);
     }
+
+    #[Route('/new', name: 'todo_new', methods: ['GET', 'POST'])]
+    public function add(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $todo = new Todo();
+        $form = $this->createForm(TodoType::class, $todo);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $todo->setCreatedAt(new \DateTime()); // Set the current date and time
+            $entityManager->persist($todo);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_task', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('task/add.html.twig', [
+            'todo' => $todo,
+            'form' => $form->createView(),
+        ]);
+}
+#[Route('/{id}/edit', name: 'todo_edit', methods: ['GET', 'POST'])]
+public function edite(Request $request, Todo $todo, EntityManagerInterface $entityManager): Response
+{
+    $form = $this->createForm(TodoType::class, $todo);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Le Todo a été mis à jour avec succès.');
+
+        return $this->redirectToRoute('app_task');
+    }
+
+    return $this->render('task/edite.html.twig', [
+        'todo' => $todo,
+        'form' => $form->createView(),
+    ]);
+}
+
+#[Route('/{id}', name: 'todo_delete', methods: ['POST'])]
+public function delete(Request $request, Todo $todo, EntityManagerInterface $entityManager): RedirectResponse
+{
+    if ($this->isCsrfTokenValid('delete'.$todo->getId(), $request->request->get('_token'))) {
+        $entityManager->remove($todo);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Le Todo a été supprimé avec succès.');
+    }
+
+    return $this->redirectToRoute('app_task');
+}
 }
---
+++ src/Form/TodoType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Todo;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+class TodoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('title', TextType::class, [
+            'label' => 'Title',
+        ])
+        ->add('description', TextareaType::class, [
+            'label' => 'Description',
+            'required' => false,
+        ])
+        ->add('CreatedAt', DateType::class, [
+            'label' => 'Description',
+            'required' => false,
+        ])
+        ->add('isCompleted', CheckboxType::class, [
+            'label' => 'Completed',
+            'required' => false,
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Todo::class,
+        ]);
+    }
+}
